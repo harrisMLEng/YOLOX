3a8771e19159aad573c146eb8625e2090b6f6ebd
harrisDLEng <harris47naeem@gmail.com>
2024-02-28 17:44:29 +0500

    change dataset directory structure

--- exps/example/custom/nano.py
+++ exps/example/custom/nano.py
@@ -24,9 +24,9 @@ class Exp(MyExp):
         self.max_epoch = 30
 
         # Define yourself dataset path
-        self.data_dir = "datasets/coco128"
-        self.train_ann = "instances_train2017.json"
-        self.val_ann = "instances_val2017.json"
+        self.data_dir = "datasets/coco"
+        self.train_ann = "train.json"
+        self.val_ann = "val.json"
 
         self.num_classes = 2
 
