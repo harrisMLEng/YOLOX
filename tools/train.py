55df555aa39ef8f48997fbb72f7f4ddd6c237287
harrisDLEng <harris47naeem@gmail.com>
2024-03-06 16:09:15 +0500

    fix: train

--- tools/train.py
+++ tools/train.py
@@ -123,15 +123,15 @@ def make_parser():
 
 @logger.catch
 def main(exp: Exp, args):
-    if exp.seed is not None:
-        random.seed(exp.seed)
-        torch.manual_seed(exp.seed)
-        cudnn.deterministic = True
-        warnings.warn(
-            "You have chosen to seed training. This will turn on the CUDNN deterministic setting, "
-            "which can slow down your training considerably! You may see unexpected behavior "
-            "when restarting from checkpoints."
-        )
+    # if exp.seed is not None:
+    #     random.seed(exp.seed)
+    #     torch.manual_seed(exp.seed)
+    #     cudnn.deterministic = True
+    #     warnings.warn(
+    #         "You have chosen to seed training. This will turn on the CUDNN deterministic setting, "
+    #         "which can slow down your training considerably! You may see unexpected behavior "
+    #         "when restarting from checkpoints."
+    #     )
 
     # set environment variables for distributed training
     # configure_nccl()
@@ -160,17 +160,17 @@ if __name__ == "__main__":
 
     num_gpu = get_num_devices() if args.devices is None else args.devices
     assert num_gpu <= get_num_devices()
+    main(exp, args)
+    # if args.cache is not None:
+    #     exp.dataset = exp.get_dataset(cache=True, cache_type=args.cache)
 
-    if args.cache is not None:
-        exp.dataset = exp.get_dataset(cache=True, cache_type=args.cache)
-
-    dist_url = "auto" if args.dist_url is None else args.dist_url
-    launch(
-        main,
-        num_gpu,
-        args.num_machines,
-        args.machine_rank,
-        backend=args.dist_backend,
-        dist_url=dist_url,
-        args=(exp, args),
-    )
+    # dist_url = "auto" if args.dist_url is None else args.dist_url
+    # launch(
+    #     main,
+    #     num_gpu,
+    #     args.num_machines,
+    #     args.machine_rank,
+    #     backend=args.dist_backend,
+    #     dist_url=dist_url,
+    #     args=(exp, args),
+    # )
